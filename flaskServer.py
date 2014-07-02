98a51ee5068cb0fb521d6c557070ac5b2a19fddf
Renato Fabbri <renato.fabbri@gmail.com>
2014-07-02 10:06:46 -0300

    feito parserzinho p votacao online

--- flaskServer.py
+++ flaskServer.py
@@ -22,6 +22,29 @@ def hello():
     text_block=string.join([str(ii) for ii in info],"<br />")
     return text_block
 
+@app.route("/votos/")
+def votos():
+    client=pymongo.MongoClient(U)
+    db = client[U.split("/")[-1]]
+    C = db[HTAG_] #collection
+    msgs=[i for i in C.find()]
+    consultas={}
+    for msg in msgs:
+        msg_=msg["text"].split(" ")
+        if msg_[2] in ("+1","-1","0"):
+            # voto
+            consulta=msg_[1]
+            if consulta in consultas.keys():
+                consultas[consulta][msg_[2]]+=1
+            else:
+                consultas[consulta]={}
+                consultas[consulta]["+1"]=0
+                consultas[consulta]["-1"]=0
+                consultas[consulta]["0"]=0
+                consultas[consulta][msg_[2]]+=1
+    return str(consultas)
+    
+    
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0')
