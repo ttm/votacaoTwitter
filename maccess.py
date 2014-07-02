ac92950bd11b83586786966e4989010f4b4de050
Renato Fabbri <renato.fabbri@gmail.com>
2014-07-02 02:32:14 -0300

    adicinados arquivos de acesso e de referencia

---
+++ maccess.py
@@ -0,0 +1,8 @@
+class mdc:
+    u1="mongodb://foo:bar@ds053419.mongolab.com:53419/votacao"
+class tw: # nome participateste
+    tak='gI4xzVF7ixUT5uwRWLDtXXMoY'
+    taks='01ZGCKWpT22dktJFoBfssa85XOCs07uDl8biMQOVuLiVrJ74Bh'
+    tat='2599160258-ViebIaDiOEdC5WVna2wwKqRqjSHFs9FD0wX43Ns'
+    tats='FxMks3AdWQwk0wrXVNwM1QeVG4KJEvPxk2qR3ZscDx8M3'
+
